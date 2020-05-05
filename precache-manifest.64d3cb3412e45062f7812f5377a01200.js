ffc2791f958ac97fa5e43e73b0b9065083469211
Andrew A. Stenipanin <and5251@yandex.ru>
2020-05-05 23:02:34 +0300

    Updates

---
+++ precache-manifest.64d3cb3412e45062f7812f5377a01200.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93d16de843e41bb18a3e7d86d681eb2",
+    "url": "/currency_app/index.html"
+  },
+  {
+    "revision": "fbe2b4952d4f71a1e5c2",
+    "url": "/currency_app/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "dce2c2a570479a7719d2",
+    "url": "/currency_app/static/css/main.6dda7e96.chunk.css"
+  },
+  {
+    "revision": "fbe2b4952d4f71a1e5c2",
+    "url": "/currency_app/static/js/2.98211b33.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/currency_app/static/js/2.98211b33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce2c2a570479a7719d2",
+    "url": "/currency_app/static/js/main.30fe5694.chunk.js"
+  },
+  {
+    "revision": "d5eaa25088660b7b96b3",
+    "url": "/currency_app/static/js/runtime-main.01f74b7e.js"
+  }
+]);
