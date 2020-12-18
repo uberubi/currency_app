23ef2dd0fc87986d05a785607f79b60e73ed38b1
Andrew A. Stenipanin <and5251@yandex.ru>
2020-12-18 13:31:11 +0300

    router fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-// import "./App.scss";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Convertor from "./pages/Convertor/Convertor";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import Header from "./components/Header/Header";
-import styles from './App.module.scss'
+import styles from "./App.module.scss";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Router>
+      <Router basename="/">
         <div className={styles.container}>
-        <Header />
+          <Header />
           <Route exact path="/" component={Home} />
           <Route exact path="/convertor" component={Convertor} />
         </div>
